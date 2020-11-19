ef5b6ce197cfef20f098eeb31043d6ea9d7b6ad5
James Mernin <jamespmernin@gmail.com>
2020-11-19 16:57:07 -0500

    James Mernin - solved algorithm

--- encrypt_phone_number.rb
+++ encrypt_phone_number.rb
@@ -1,8 +1,9 @@
 
 # RUBY ENCRYPT PHONE NUMBER
+# Found a way to put just the last four characters in a string from here: https://stackoverflow.com/questions/2174767/extracting-the-last-n-characters-from-a-ruby-string
 
 def encrypt_phone_number(str)
-
+  return '***-***-' + str.chars.last(4).join
 end
 
 puts encrypt_phone_number '718-867-5309'
